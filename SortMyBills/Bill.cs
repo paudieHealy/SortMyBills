2923be26551b76386dfd0d28ba6b2c5b759f71de
Paudie Healy <paudiehealy@hotmail.com>
2016-11-21 14:52:08 +0000

    test for new

--- SortMyBills/Bill.cs
+++ SortMyBills/Bill.cs
@@ -12,7 +12,7 @@ namespace SortMyBills
         public char getBillDue()
         {
             throw new NotImplementedException();
-            //this is a test by paudie hellooooooooooo
+            //this is a test by me
         }
 
         public void setBillDue(char billDue)
